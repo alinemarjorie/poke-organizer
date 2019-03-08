47214398a18fe61756aa0142f65aed90ee042e68
alinemarjorie <aline.marjorie.af@gmail.com>
2019-03-08 14:56:37 -0300

    Filtro realizado por onclick

--- src/main.js
+++ src/main.js
@@ -1,45 +1,57 @@
-function showPercent(filteredPokemon) {
-    let number = filteredPokemon.length / 151 * 100;
-    document.getElementById("math-pokemons").innerHTML =
-        `<div class="math"> ${Math.round(number)}% 
-    </div>
-`
+let pokemons = [];
+
+window.onload = function () {
+    pokemons = POKEMON.pokemon;
+    showPokemons();
 }
 
-function showPokemons(filteredPokemon) {
-    document.getElementById("pokemons").innerHTML = `${filteredPokemon.map(pokemon => `
+function showPokemons() {
+    document.getElementById("pokemons").innerHTML = `${pokemons.map(pokemon => `
         <div class="each-pokemon">
         <img src="${pokemon.img}" class="pokemon-img"/>
         <div class="text-name">
             <h3 class="pokemon-name">${pokemon.name}</h3>
         </div>
         </div>
-    `
-    ).join('')
- }
+   `).join('')}`
+    getPokemonOnClick()
+}
+
+function showPercent() {
+    let number = pokemons.length / 151 * 100;
+    document.getElementById("math-pokemons").innerHTML =
+        `<div class="math"> ${Math.round(number)}% 
+    </div>
 `
 }
 
 const selectTypeElement = document.getElementById("select-type");
-selectTypeElement.addEventListener("change", typeFilter);
+const selectWeaknessesElement = document.getElementById("select-weaknesses");
+const filterButtonElement = document.getElementById("filter")
+filterButtonElement.addEventListener("click", callFilter)
 
-function typeFilter() {
-    let filteredPokemon = POKEMON.pokemon.filter(pokemon => {
-        return (pokemon.type.includes(selectTypeElement.value));
-    });
-    showPercent(filteredPokemon);
-    showPokemons(filteredPokemon);
+function callFilter() {
+    let type = selectTypeElement.value;
+    let weaknesses = selectWeaknessesElement.value;
+    pokemons = POKEMON.pokemon.filter(pokemon => {
+        if (type != "" && weaknesses != "") {
+            return (pokemon.weaknesses.includes(weaknesses) && pokemon.type.includes(type));
+        }
+        if (type != "") {
+            return (pokemon.type.includes(type));
+        }
+        if (weaknesses != "") {
+            return (pokemon.weaknesses.includes(weaknesses));
+        }
+    })
+    showPercent();
+    showPokemons();
+    resetSelectElement();
 }
 
-const selectWeaknessesElement = document.getElementById("select-weaknesses");
-selectWeaknessesElement.addEventListener("change", weaknessesFilter);
-
-function weaknessesFilter() {
-    let filteredPokemon = POKEMON.pokemon.filter(pokemon => {
-        return (pokemon.weaknesses.includes(selectWeaknessesElement.value));
-    })
-    showPercent(filteredPokemon);
-    showPokemons(filteredPokemon);
+function resetSelectElement() {
+    selectTypeElement.selectedIndex = 0; 
+    selectWeaknessesElement.selectedIndex = 0; 
 }
 
 const pokeSearchElement = document.getElementById("poke-search");
@@ -47,29 +59,57 @@ pokeSearchElement.addEventListener("keyup", pokemonSearch);
 
 function pokemonSearch() {
     let searchUpper = pokeSearchElement.value.toUpperCase();
-    let detailsPokemon = POKEMON.pokemon.filter(pokemon => {
+    pokemons = POKEMON.pokemon.filter(pokemon => {
         let nameUpper = pokemon.name.toUpperCase();
         return (nameUpper.includes(searchUpper));
     })
-    showFullPokemon(detailsPokemon);
+    showPokemons();
+    document.getElementById("math-pokemons").style.display = "none";
 }
 
-function showFullPokemon(detailsPokemon) {
-    document.getElementById("pokemons").innerHTML = `${detailsPokemon.map(pokemon => `
-    <div class="each-full-pokemon">
-    <img src="${pokemon.img}" class="pokemon-img"/>
-    <div class="text-name">
-        <h3 class="pokemon-number">${pokemon.num}</h3>
-        <h3 class="pokemon-name">${pokemon.name}</h3>
-        <p><strong>Tipo:</strong> ${pokemon.type}</p>
-        <p><strong>Fraquezas:</strong> ${pokemon.weaknesses}</p>
-        <p><strong>Horário:</strong> ${pokemon.spawn_time}</p>     
-        <p><strong>Altura:</strong> ${pokemon.height}</p>
-        <p><strong>Peso:</strong> ${pokemon.weight}</p>
-    </div>
-    </div>
-`
-).join('')
+function getPokemonOnClick() {
+    let pokemonList = document.querySelectorAll('.pokemon-img');
+
+    for (let pokemon of pokemonList) {
+        pokemon.addEventListener('click', function (event) {
+            console.log("ae")
+        })
+    }
 }
-`
+
+const AZButton = document.getElementById("aToZ");
+AZButton.addEventListener("click", orderAZ);
+
+function orderAZ() {
+    pokemons = pokemons.sort((a, b) => {
+        if (a.name > b.name) {
+            return 1;
+        }
+        if (a.name < b.name) {
+            return -1;
+        }
+        return 0;
+    });
+    showPokemons();
 }
+
+// Modal:
+// function showFullPokemon(detailsPokemon) {
+//     document.getElementById("pokemons").innerHTML = `${detailsPokemon.map(pokemon => `
+//     <div class="each-full-pokemon">
+//     <img src="${pokemon.img}" class="pokemon-img"/>
+//     <div class="text-name">
+//         <h3 class="pokemon-number">${pokemon.num}</h3>
+//         <h3 class="pokemon-name">${pokemon.name}</h3>
+//         <p><strong>Tipo:</strong> ${pokemon.type}</p>
+//         <p><strong>Fraquezas:</strong> ${pokemon.weaknesses}</p>
+//         <p><strong>Horário:</strong> ${pokemon.spawn_time}</p>     
+//         <p><strong>Altura:</strong> ${pokemon.height}</p>
+//         <p><strong>Peso:</strong> ${pokemon.weight}</p>
+//     </div>
+//     </div>
+// `
+// ).join('')
+// }
+// `
+// }
